f003c3dd58da92f10f81fe4c96361ba7c6a4e4bc
Alex Bumbu <bmbalex@gmail.com>
2016-12-17 00:36:25 +0000

    Init. Add a prototype HTML structure

---
+++ README.md
@@ -0,0 +1,16 @@
+# Perform test app
+
+## The task
+
+Build a UI to search the Movie DB: The user should be able to enter some text into a
+search field, see and browse the results from the Movie DB.
+
+Anything else it does it up to you!
+
+You can use any language, library or tool you feel comfortable with.
+
+## Start-up
+  * `npm install` - to install dependencies
+  * `npm start` - starts local dev server
+  * `npm run build` - builds production ready version
+  * `npm test` - run tests
---
+++ gulpfile.js
@@ -0,0 +1,93 @@
+// Sort alphabetically
+const gulp = require('gulp')
+const plumber = require('gulp-plumber')
+const sourcemaps = require('gulp-sourcemaps')
+const webpackStream = require('webpack-stream')
+
+const PATHS = {
+  SRC: 'src',
+  JS: {
+    SRC: 'src/js/',
+    DIST: 'public/js/',
+  },
+}
+
+/**
+ * Scripts
+ */
+
+gulp.task('dev:scripts', function() {
+  return gulp.src(PATHS.JS.SRC + 'index.js')
+    .pipe(webpackStream({
+      devtool: 'inline-source-map',
+      output: {
+        filename: 'index.js',
+      },
+      module: {
+        loaders: [
+          {
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            include: __dirname,
+            query: {
+              presets: ["react", "es2015"],
+            }
+          }
+        ]
+      },
+    }))
+    .on('error', function handleError() {
+      this.emit('end'); // Recover from errors
+    })
+    .pipe(gulp.dest(PATHS.JS.DIST))
+})
+
+gulp.task('build:scripts', function() {
+  return gulp.src(PATHS.JS.SRC + 'index.js')
+    .pipe(webpackStream({
+      output: {
+        filename: 'index.js',
+      },
+      module: {
+        loaders: [
+          {
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            include: __dirname,
+            query: {
+              presets: ["react", "es2015"],
+            }
+          }
+        ]
+      },
+      plugins: [
+        new webpackStream.webpack.optimize.UglifyJsPlugin({
+          compress: {
+              warnings: false
+          }
+        })
+      ]
+    }))
+    .pipe(gulp.dest(PATHS.JS.DIST))
+})
+
+/**
+ * Server
+ */
+
+gulp.task('dev:server', function() {
+  const server = require('./server/index')
+})
+
+/**
+ * Watch
+ */
+
+gulp.task('dev:watch', function() {
+  gulp.watch([PATHS.JS.SRC + '**/*.js'], ['dev:scripts']);
+})
+
+gulp.task('default', ['dev:scripts', 'dev:server', 'dev:watch'])
+gulp.task('build', ['build:scripts'])
