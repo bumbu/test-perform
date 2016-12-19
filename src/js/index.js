0ff06a91be20523c63cc8187bf0b7ebbac81a820
Alex Bumbu <bmbalex@gmail.com>
2016-12-19 21:37:46 +0000

    Bind all modules together, render the app

--- src/js/index.js
+++ src/js/index.js
@@ -5,15 +5,24 @@ import Store from './modules/store'
 import Search from './modules/search'
 import { getActions } from './modules/actions'
 import { bindActions } from './modules/utils'
+import { reducer } from './modules/reducer'
+import request from 'superagent'
+import { SEACH_API, API_KEY } from './config'
 
-const store = new Store(() => {}, {})
-const search = new Search()
+const defaultState = {
+  items: [],
+  isLoading: false,
+  keyword: '',
+}
+const store = new Store(reducer, defaultState)
+const search = new Search(request, SEACH_API, API_KEY)
 const { searchBy, loadMore } = getActions(search)
 const actions = bindActions({searchBy, loadMore}, store.dispatch, store.getState)
 
 function render() {
+  const state = store.getState()
   ReactDOM.render(
-    <App {...actions} {...store.getState()}/>,
+    <App {...actions} state={state}/>,
     document.getElementById('root')
   )
 }
