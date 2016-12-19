93c2e6d18f22f8927396323b6f5410172c336e33
Alex Bumbu <bmbalex@gmail.com>
2016-12-19 21:47:30 +0000

    Add tests for App component

--- src/js/components/App.test.js
+++ src/js/components/App.test.js
@@ -1,4 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 import { shallow, mount, render } from 'enzyme'
+import Header from './Header'
+import Listing from './Listing'
+
+describe('(App)', () => {
+  const noop = () => {}
+  const obj = {}
+
+  it('renders', () => {
+    const wrapper = shallow(<App searchBy={noop} loadMore={noop} state={obj} />);
+  })
+
+  it('contains Header and Listing components', () => {
+    const wrapper = shallow(<App searchBy={noop} loadMore={noop} state={obj} />);
+    expect(wrapper.find(Header).length).toBe(1)
+    expect(wrapper.find(Listing).length).toBe(1)
+  })
+})
